3f2e76eb30f950cb1de20314f1d6d14875d9f786
Anto <anto.christo.20@gmail.com>
2017-02-10 23:54:58 +0530

    Edited index.html

--- public/javascripts/space.js
+++ public/javascripts/space.js
@@ -345,7 +345,7 @@ var flag_sw=0;
       planets = universe.add.group();
       planets.enableBody = true;
 
-      for(var i=0;i<400;i++){
+      for(var i=0;i<1000;i++){
 
         if(i%3==0)
         pl[i] = planets.create(1000 , 300, 'pn1');
